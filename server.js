756e19dd479b8e855864cd12932618d6a2de3e43
Rafael Mendes <mafaelrendes8080@gmail.com>
2020-03-25 17:12:14 -0300

    ...

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
+
+// Permite a plicação retornar json
 app.use(express.json());
+
+// Torna api publica
 app.use(cors());
 
 // Iniciando o DB
